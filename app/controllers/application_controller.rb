c390f9f12d2c6923dae976f5da9866b22b7e3726
Samuel Cornet <samuel@noukod.com>
2019-01-11 17:00:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
   get '/' do
     erb :index
+  end
+
+  
+  get '/recipes' do
+      @recipe = Recipe.create(name => params[:name], ingredients =>[:ingredients], cook_time => params[:cook_time])
   end
 end
