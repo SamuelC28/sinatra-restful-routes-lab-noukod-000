adca5c24b73bfaabd1e13b5d98b587636cc66ee7
Samuel Cornet <samuel@noukod.com>
2019-01-11 17:41:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,14 @@ class ApplicationController < Sinatra::Base
     @recipes = Recipe.all
     erb :index
   end
-  
+
   get '/recipes/new' do
     erb :new
   end
-  
+
   post '/recipes' do
     recipe = Recipe.create name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]
-      
+
     redirect "/recipes/#{recipe.id}"
   end
 end
