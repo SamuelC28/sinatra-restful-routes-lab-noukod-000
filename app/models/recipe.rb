77816eaeb30ae3145e9d4a918d0d0800a715b20c
Samuel Cornet <samuel@noukod.com>
2019-01-11 16:29:43 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,4 @@
 #Placeholder for a model
 class Recipe < ActiveRecord::Migration
-  def change
-    create_table :recipes do |t|
-      t.string :name
-      t.string :ingredients
-      t.date : ...
-    end
-  end
+  
 end
