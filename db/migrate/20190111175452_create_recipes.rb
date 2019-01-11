0166c6227b8ec7ace17914aff6f314b4a1c95243
Samuel Cornet <samuel@noukod.com>
2019-01-11 17:55:00 +0000

    Automatically backed up by Learn

--- db/migrate/20190111175452_create_recipes.rb
+++ db/migrate/20190111175452_create_recipes.rb
@@ -1,4 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |column|
+      column.string "name"
+      column.string "ingredients"
+      column.string "cook_time"
   end
 end
